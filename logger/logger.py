58ca060153ef95bb6c6fc3ded7414ebaa87761bd
Daryl Dang <daryl.dang4@gmail.com>
2021-07-20 23:22:29 -0600

    Add verbosity level 1 control to logger
    Add verbosity level 1 control to logger which allows console
    printing.

--- logger/logger.py
+++ logger/logger.py
@@ -8,6 +8,8 @@
 ###########
 import logging
 
+from enum import IntEnum, auto
+
 #############
 # CONSTANTS #
 #############
@@ -16,6 +18,17 @@ import logging
 # GLOBALS #
 ###########
 
+#####################
+# Class Definitions #
+#####################
+class VerbosityLevel(IntEnum):
+    """
+    Enum for verbosity levels.
+    """
+    VERBOSITY_LEVEL0 = 0
+    VERBOSITY_LEVEL1 = auto()
+    VERBOSITY_LEVEL2 = auto()
+
 class Logger:
     """
     Singleton class of the logger system that will handle the logging system of the game.
@@ -23,10 +36,13 @@ class Logger:
 
     Attributes
     ----------
-    _logger_instance : Logger
-        Represents the current running instance of Logger, this will only be created once.
+    _logger_instance: Logger
+        Represents the current running instance of Logger, this will only be created once (by default set to None).
+    _verbosity_level: int
+        Represents the verbosity level (by default set to VERBOSITY_LEVEL0).
     """
     _logger_instance = None
+    _verbosity_level = int(VerbosityLevel.VERBOSITY_LEVEL0)
 
     @staticmethod
     def get_instance():
@@ -45,3 +61,43 @@ class Logger:
             raise Exception("{}: Cannot construct, an instance is already running.".format(__file__))
         else:
             Logger._logger_instance = self
+
+    def set_verbosity_level(self, verbosity_level: int) -> bool:
+        """
+        Sets the verbosity level of logger.
+
+        Args:
+            verbosity_level (int): Level of verbosity that will be set to logger.
+
+        Returns:
+            bool: Returns if verbosity level was set successfully.
+        """
+        _enum_values = [item.value for item in VerbosityLevel]
+        if verbosity_level in _enum_values:
+            self._verbosity_level = verbosity_level
+            return True
+        else:
+            return False
+
+    def print_console(self, message: str="", src_file: str="") -> None:
+        """
+        Prints to console if verbosity level 1 or more set.
+
+        Args:
+            message (str, optional)     : Message to print to console. Defaults to "".
+            src_file (str, optional)    : [description]. Defaults to "".
+        """
+        if self._verbosity_level >= int(VerbosityLevel.VERBOSITY_LEVEL1):
+            print(message)
+
+if __name__ == "__main__":
+    pass
+    # logger = Logger.get_instance()
+
+    # #Testing verbosity level 0
+    # logger.set_verbosity_level(0)
+    # logger.print_console("testing logger verbosity level 0")
+
+    # #Testing verbosity level 1
+    # logger.set_verbosity_level(1)
+    # logger.print_console("testing logger verbosity level 1")
